198c2ce8fb387b7d9a1e33d4c458b11c3c183b2f
jggruber247 <jggruber247@gmail.com>
2019-07-21 12:51:16 -0400

    First commit

---
+++ WonderTask/ical_days_parse.py
@@ -0,0 +1,24 @@
+#!python
+from icalendar import Calendar
+import datetime
+import requests
+import sys
+events=[]
+now=(datetime.date.today())
+try:
+	url=sys.argv[1]
+except IndexError:
+	print("No URL given...")
+	sys.exit()
+try:
+	cal = Calendar.from_ical(requests.get(url).text)
+except:
+	print("Can't fetch .ics file, sorry...")
+	sys.exit()
+for e in cal.walk('vevent'):
+    delta=e.get('dtstart').dt-now
+    event=[e.get('summary'),(delta.days)]
+    events.append(event)
+events.sort(key=lambda x:x[1])
+for e in events:
+	print('' + int(e[1]))
